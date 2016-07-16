3248327efa860f2ae5f1f0ee8ae862a394423d50
Zay Yar Wai Phyo <z@codeboratory.com>
2016-07-16 20:55:14 +0800

    Initial Commit

---
+++ Daunglan/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Daunglan
+//
+//  Created by Zay Yar Wai Phyo on 16/7/16.
+//  Copyright © 2016 Kyansit Creatives. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Daunglan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Daunglan
+//
+//  Created by Zay Yar Wai Phyo on 16/7/16.
+//  Copyright © 2016 Kyansit Creatives. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
